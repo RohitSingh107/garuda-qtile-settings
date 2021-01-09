1aaea698e5464677bf212674d3d629b716b2dd44
Rohit Singh <RohitSinghEmail@protonmail.com>
2021-01-09 12:30:17 +0530

    added a cli application to manage directories for pywal

--- etc/skel/.config/qtile/scripts/pywal-colors.py
+++ etc/skel/.config/qtile/scripts/pywal-colors.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 import os
 import random
-wallpaper_list = []
-
-d = "/usr/share/wallpapers/garuda-wallpapers"
-for path in os.listdir(d):
-    full_path = os.path.join(d, path)
-    if os.path.isfile(full_path):
-        wallpaper_list.append(full_path)
-
-#Add your own wallpapers directory
-#e = "/home/rohit/mydata/Pictures/wallpapers"
-#for path in os.listdir(e):
-#    full_path = os.path.join(e, path)
-#    if os.path.isfile(full_path):
-#        wallpaper_list.append(full_path)
+import wallist
+import sys
 
 
-wallpaper = random.choice(wallpaper_list)
 
 
-os.system("wal -i " + wallpaper)
 
-#print(wallpaper_list)
+if len(sys.argv) > 1:
+    if sys.argv[1] == "--dirs":
+        wallist.print_dirs()
+    elif sys.argv[1] == "--add":
+        dir_path = input("Enter full path of directory: ")
+        with open("wallist.py", "a") as f:
+            f.write('\nadd_dir("{}")'.format(dir_path))
+else:         
+    wallpaper = random.choice(wallist.wallpaper_list)
+    os.system("wal -i " + wallpaper)
+
+#print(wallist.wallpaper_list)
+
---
+++ etc/skel/.config/qtile/scripts/wallist.py
@@ -0,0 +1,17 @@
+import os
+
+wallpaper_list = []
+dir_list = []
+
+def add_dir(dir_path):
+    dir_list.append(dir_path)
+    for path in os.listdir(dir_path):
+        full_path = os.path.join(dir_path, path)
+        if os.path.isfile(full_path):
+            wallpaper_list.append(full_path)
+def print_dirs():
+    print(dir_list)
+
+add_dir("/usr/share/wallpapers/garuda-wallpapers")
+
+add_dir("/home/rohit/mydata/Pictures/wallpapers")
