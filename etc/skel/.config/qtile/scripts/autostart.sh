aea05790b68483edf9569a0d9a627e398e523f6e
Rohit <RohitSinghEmail@protonmail.com>
2022-05-13 13:40:32 +0530

    qtile minor change

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -20,7 +20,8 @@ blueman-applet &
 picom --config .config/picom/picom-blur.conf --experimental-backends &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
-feh --randomize --bg-fill /usr/share/wallpapers/garuda-wallpapers/*
+# feh --randomize --bg-fill /usr/share/wallpapers/garuda-wallpapers/*
+feh --bg-fill /usr/share/wallpapers/garuda-wallpapers/qtile.jpg
 #starting user applications at boot time
 run volumeicon &
 #run discord &
