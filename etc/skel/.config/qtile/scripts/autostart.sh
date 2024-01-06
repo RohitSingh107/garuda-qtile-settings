c99ca32ed2ae33bd2644a62ae47539bddd1aad74
jastx-jasmint <jastxakajasmineteax@gmail.com>
2024-01-06 21:46:21 +1100

    Fix picom start command, Alacritty migration

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -19,7 +19,7 @@ numlockx on &
 blueman-applet &
 #flameshot &
 #picom --config $HOME/.config/picom/picom.conf &
-picom --config .config/picom/picom-blur.conf --experimental-backends &
+picom --config .config/picom/picom-blur.conf &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
 # feh --randomize --bg-fill /usr/share/wallpapers/garuda-wallpapers/*
